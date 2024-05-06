c73f9d87289c5548ecac38788837515ad512d66c
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-05-06 17:49:06 +0400

    add: currenct converter logic and display view

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ currencies.forEach((currency) => {
   option.text = currency;
   toDropDown.add(option);
 });
+
+fromDropDown.value = "USD";
+toDropDown.value = "INR";
+
+let convertCurrency = () => {
+  const amount = document.querySelector("#amount").value;
+  const fromCurrency = fromDropDown.value;
+  const toCurrency = toDropDown.value;
+
+  if (amount.length != 0) {
+    fetch(api)
+      .then((resp) => resp.json())
+      .then((data) => {
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+          2
+        )} ${toCurrency}`;
+      });
+  } else {
+    alert("Please fill in the amount");
+  }
+};
+
+document
+  .querySelector("#convert-button")
+  .addEventListener("click", convertCurrency);
+window.addEventListener("load", convertCurrency);
