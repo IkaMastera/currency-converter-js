3e5c883076840e61a7776af0d78be30c1fe4493c
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-05-06 17:31:51 +0400

    add: dropDown from our currencies

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 const fromDropDown = document.getElementById("from-currency-select");
 const toDropDown = document.getElementById("to-currency-select");
+
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
+});
